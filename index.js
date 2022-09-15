b2950b324c044d83b70ae3f3aa1b3d63dbe813b3
2Tough <vainferro@gmail.com>
2022-09-16 00:27:09 +0300

    Added object to store changing values.

--- index.js
+++ index.js
@@ -1,33 +1,62 @@
-let selectedColor = document.getElementById('color-selected')
+let colorWheel = document.getElementById('color-selected')
+let selectColor = document.getElementById("colorsTo")
+let getColor = document.getElementById('box-1')
 
+let colorObject = {
+    input: "monochrome",
+    wheel: 000000
+}
 
+let optionArray = []
+let colorArray = []
+
+let value = selectColor.value
 let options = {
-    method: 'GET'
-    
+    method: 'GET',
 }
 
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB', options)
-    .then(message => message.json())
-    .then(data => console.log(data))
-    
-    console.log(colorsTo)
-    
-    
-let selectColor = document.getElementById("colorsTo");
+// For input value
 function onChange() {
-  let value = selectColor.value;
-  let text = selectColor.options[selectColor.selectedIndex].text;
-  console.log(value, text);
-}
+    let value = selectColor.value
+    let text = selectColor.options[selectColor.selectedIndex].text;
+    
+    optionArray.push(value)
+    colorObject.input = value
+//    console.log(optionArray[optionArray.length-1])
+    
+    fetchColor()
+    }
+    
 selectColor.onchange = onChange;
 onChange();
 
-console.log(selectedColor.value)
 
-selectedColor.addEventListener('input', function() {
-    console.log(selectedColor.value)
-})
+// For color wheel
+colorWheel.addEventListener('input', colorSelect)
 
+function colorSelect() {
+    let wheel = colorWheel.value
+    let colorChar = wheel.substr(wheel.length - 6)
+    
+    colorObject.wheel = colorChar
+    
+//    colorArray.push(colorChar)
+//    console.log(colorArray[optionArray.length-1])
+    
+    
+    fetchColor()
+    
+
+    
+}
+
+
+function fetchColor(colorChar) {
+fetch(`https://www.thecolorapi.com/scheme?hex=${colorObject.wheel}&mode=${colorObject.input}&count=5`, options)
+    .then(message => message.json())
+    .then(data => console.log(data))
+    
+}
 
 
 
