2d55cd76feaf48753225f0e2129d03d5486b9113
2Tough <vainferro@gmail.com>
2022-09-16 23:49:47 +0300

    Changes to css and js.
    Removed default color html to object color. Completed reflecting selected color API to containers and changed css for more neutral look.

--- index.js
+++ index.js
@@ -20,15 +20,12 @@ function onChange() {
     let value = selectColor.value
     let text = selectColor.options[selectColor.selectedIndex].text;
     
-    optionArray.push(value)
     colorObject.input = value
-//    console.log(optionArray[optionArray.length-1])
-    
     fetchColor()
     }
-    
-selectColor.onchange = onChange;
-onChange();
+        
+    selectColor.onchange = onChange;
+    onChange();
 
 
 // For color wheel
@@ -39,24 +36,27 @@ function colorSelect() {
     let colorChar = wheel.substr(wheel.length - 6)
     
     colorObject.wheel = colorChar
-    
-//    colorArray.push(colorChar)
-//    console.log(colorArray[optionArray.length-1])
-    
-    
     fetchColor()
-    
-
-    
-}
+    }
 
 
 function fetchColor(colorChar) {
 fetch(`https://www.thecolorapi.com/scheme?hex=${colorObject.wheel}&mode=${colorObject.input}&count=5`, options)
     .then(message => message.json())
-    .then(data => console.log(data))
+    .then(data => {
+     
     
-}
-
-
-
+        document.getElementById('first-color').style.background = data.colors[0].hex.value 
+        document.getElementById('second-color').style.background = data.colors[1].hex.value 
+        document.getElementById('third-color').style.background = data.colors[2].hex.value 
+        document.getElementById('fourth-color').style.background = data.colors[3].hex.value 
+        document.getElementById('fifth-color').style.background = data.colors[4].hex.value 
+        
+        document.getElementById('one').textContent = data.colors[0].hex.value  
+        document.getElementById('two').textContent = data.colors[1].hex.value 
+        document.getElementById('three').textContent = data.colors[2].hex.value 
+        document.getElementById('four').textContent = data.colors[3].hex.value 
+        document.getElementById('five').textContent = data.colors[4].hex.value 
+    
+        }
+        )}
